2456f93b093f839b4c1a95fd4cfb8ecce5c81fed
javi <fordrsmax@gmail.com>
2023-11-11 20:13:58 +0100

    ADD dinamic view about contact 2 in 1

--- controllers/ejercicio1/about.controller.js
+++ controllers/ejercicio1/about.controller.js
@@ -1,5 +1,6 @@
 const getAbout = (req,res)=>{
-    res.render('./ejercicio1/about')
+    //res.render('./ejercicio1/about')
+    res.render('./ejercicio1/aboutContact2in1',{render:'about'})
 }
 
 module.exports = {
--- controllers/ejercicio1/contact.controller.js
+++ controllers/ejercicio1/contact.controller.js
@@ -1,5 +1,12 @@
 const getMembers = (req,res) => {
-    res.render('./ejercicio1/contact',{
+    /*res.render('./ejercicio1/contact',{
+        imageJavi: 'https://avatars.githubusercontent.com/u/6449798?v=4',
+        imageAlejandro: 'https://avatars.githubusercontent.com/u/144797221?v=4',
+        githubJavi: 'https://github.com/javirs200',
+        githubAlejandro: 'https://github.com/alejandroFMA'
+    })*/
+    res.render('./ejercicio1/aboutContact2in1',{
+        render:'contact',
         imageJavi: 'https://avatars.githubusercontent.com/u/6449798?v=4',
         imageAlejandro: 'https://avatars.githubusercontent.com/u/144797221?v=4',
         githubJavi: 'https://github.com/javirs200',
