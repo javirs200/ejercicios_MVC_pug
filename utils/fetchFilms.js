b2f0f486c0bc3d68e815f85cf34633fda0190046
javi <fordrsmax@gmail.com>
2023-11-12 12:23:17 +0100

    give error to catch log

--- utils/fetchFilms.js
+++ utils/fetchFilms.js
@@ -7,13 +7,14 @@ const apiKey = process.env.API_KEY;
 async function getFilm(id=""){
     try {
         console.log('llamada api ');
-        let response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${id}`).catch((error)=>{
+        let response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${id}`)
+        .catch((error)=>{
             console.log('internal catch error' , error);
         });
         let film = await response.json();
         return film;
     } catch (error) {
-        console.log('error llamada api')
+        console.log('error llamada api' , error)
     }
     
 };
